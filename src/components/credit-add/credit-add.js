d16d871ff483830eefbdfd100960f826b51e4e4c
LxndrOghma <Brikman424@gmail.com>
2021-08-14 18:45:51 +0300

    Add credit-offer section

---
+++ src/components/credit-add/credit-add.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './credit-add.scss';
+
+function CreditAdd() {
+  return (
+    <section className='credit-add'>
+      <div className='credit-add__wrapper'>
+        <h2 className='credit-add__header'>Лига Банк</h2>
+        <h3 className='credit-add__slogan'>Кредиты на любой cлучай</h3>
+        <a className='credit-add__link' href='/'>Рассчитать кредит</a>
+      </div>
+    </section>
+  );
+}
+
+export default CreditAdd;
--- src/components/pages/main-page/main-page.js
+++ src/components/pages/main-page/main-page.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CreditAdd from '../../credit-add/credit-add';
 import PageFooter from '../../footer/page-footer/page-footer';
 import PageHeader from '../../header/page-header/page-header';
 import './main-page.scss';
@@ -7,7 +8,10 @@ function MainPage() {
   return (
     <div className='container'>
       <PageHeader />
-      <main></main>
+      <main>
+        <h1 className='visually-hidden'>ЛИГА Банк</h1>
+        <CreditAdd />
+      </main>
       <PageFooter />
     </div>
   );
