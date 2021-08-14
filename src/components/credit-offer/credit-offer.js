f0f801d2a1eef357f8f6872aedf06fe4b45d24af
LxndrOghma <Brikman424@gmail.com>
2021-08-14 23:56:51 +0300

    Rename credit-offer classes

--- src/components/credit-offer/credit-offer.js
+++ src/components/credit-offer/credit-offer.js
@@ -3,11 +3,11 @@ import './credit-offer.scss';
 
 function CreditOffer() {
   return (
-    <section className='credit-add'>
-      <div className='credit-add__wrapper'>
-        <h2 className='credit-add__header'>Лига Банк</h2>
-        <h3 className='credit-add__slogan'>Кредиты на любой cлучай</h3>
-        <a className='credit-add__link' href='/'>Рассчитать кредит</a>
+    <section className='credit-offer'>
+      <div className='credit-offer__wrapper'>
+        <h2 className='credit-offer__header'>Лига Банк</h2>
+        <h3 className='credit-offer__slogan'>Кредиты на любой cлучай</h3>
+        <a className='credit-offer__link' href='/'>Рассчитать кредит</a>
       </div>
     </section>
   );
